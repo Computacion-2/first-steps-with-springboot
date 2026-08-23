4081d5244f41ad24d8984922bacd314ed7357e7a
Mr-Trade-vs <stevanandradebecerra@gmail.com>
2026-08-23 12:50:41 -0500

    feat(publisher): added method that allows relate a track with a artist

--- demo/src/main/java/com/discography/demo/repository/PublisherRepository.java
+++ demo/src/main/java/com/discography/demo/repository/PublisherRepository.java
@@ -1,6 +1,7 @@
 package com.discography.demo.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,5 +10,7 @@ import com.discography.demo.model.Publisher;
 public interface PublisherRepository extends JpaRepository<Publisher, String> {
 
     List<Publisher> findByArtistName(String name);
+
+    Optional<Publisher> findTopByOrderByIdDesc();
     
 }
--- demo/src/main/java/com/discography/demo/service/PublisherService.java
+++ demo/src/main/java/com/discography/demo/service/PublisherService.java
@@ -1,10 +1,15 @@
 package com.discography.demo.service;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
+import com.discography.demo.model.Artist;
 import com.discography.demo.model.Publisher;
+import com.discography.demo.model.Track;
 import com.discography.demo.repository.PublisherRepository;
 
 @Service
@@ -18,5 +23,24 @@ public class PublisherService {
 
     public List<Publisher> findTracksByNameArtist(String nameArtist) {
         return publisherRepository.findByArtistName(nameArtist);
+    }
+
+    private void addTrackToArtist(Artist artist, Track song) {
+        Publisher publisherToAdd = new Publisher(idGeneration(), artist, song, LocalDate.now());
+        publisherRepository.save(publisherToAdd);
+    }
+
+    private String idGeneration() {
+
+        Optional<Publisher> lastPublisher = publisherRepository.findTopByOrderByIdDesc();
+
+        if (lastPublisher.isEmpty()) return "P00001";
+
+        String idPublisher = lastPublisher.get().getIdPublisher();
+
+        int updateId = Integer.parseInt(idPublisher.substring(1));
+        updateId++;
+
+        return String.format("P%04d", updateId);
     }
 }
